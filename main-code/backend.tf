d815c18a4e2ff0b6741e8584dcc92e0f862f7ab0
gaurav.rughani@gmail.com <130984781+rughanigaurav@users.noreply.github.com>
2024-03-30 17:47:22 +0530

    created VPC module (main.tf output.tf variable.tf) created main-code (main.tf variable.tf backend.tf) created nat-gateway module (main.tf varibale.tf)

---
+++ main-code/backend.tf
@@ -0,0 +1,10 @@
+# terraform {
+#   backend "s3" {
+
+#     bucket = "testing-bucket"
+#     key = "main-code.tfstate"
+#     region = "us-east-1"
+#     profile = "default"
+    
+#   }
+# }
---
+++ main-code/main.tf
@@ -0,0 +1,20 @@
+provider "aws" {
+
+    region  = var.region
+    profile = "default"  
+}
+
+
+module "vpc" {
+
+    source                      =   "../modules/vpc"
+    environment                 =   var.environment
+    region                      =   var.region
+    project_name                =   var.project_name
+    vpc_cidr                    =   var.vpc_cidr
+    public_subnet_az1_cidr      =   var.public_subnet_az1_cidr
+    public_subnet_az2_cidr      =   var.public_subnet_az2_cidr
+    private_subnet_az1_cidr     =   var.private_subnet_az1_cidr
+    private_subnet_az2_cidr     =   var.private_subnet_az2_cidr
+  
+}
