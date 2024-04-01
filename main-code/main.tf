6082dde3e20905377277fc80d12ac01233b75842
gaurav.rughani@gmail.com <130984781+rughanigaurav@users.noreply.github.com>
2024-04-01 10:36:44 +0530

    added Nat-Gateway in main.tf

--- main-code/main.tf
+++ main-code/main.tf
@@ -26,3 +26,15 @@ module "acm" {
     alternative_name            = var.alternative_name
   
 }
+
+module "nat-gateway" {
+
+    source = "../modules/nat-gateway"
+    public_subnet_az1_id    = var.public_subnet_az1_id
+    public_subnet_az2_id    = var.public_subnet_az2_cidr
+    internet_gateway        = var.internet_gateway
+    vpc_id                  = var.vpc_id
+    private_subnet_az1_id   = var.private_subnet_az1_id
+    private_subnet_az2_id   = var.private_subnet_az2_id
+
+}
