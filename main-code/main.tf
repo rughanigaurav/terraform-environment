ff4b713fb1b272dab401b8435a174e64377782a6
gaurav.rughani@gmail.com <130984781+rughanigaurav@users.noreply.github.com>
2024-04-12 18:55:19 +0530

    Added S3 bucket in main.tf file to fetch configuration in main-code

--- main-code/main.tf
+++ main-code/main.tf
@@ -62,9 +62,16 @@ module "ec2" {
 module "rds" {
     source = "../modules/rds"
     vpc_id = var.vpc_id
+    
     ami = var.ami
     private_subnet_az1_cidr = var.private_subnet_az1_cidr
     private_subnet_az2_cidr = var.private_subnet_az2_cidr
     public_subnet_az1_cidr = var.public_subnet_az1_cidr
     public_subnet_az2_cidr = var.private_subnet_az2_cidr
+}
+
+module "s3-bucket" {
+
+    source = "../modules/s3-bucket"
+  
 }
